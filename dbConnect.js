252dbfe1f7581c738a1a5d1228376810bdca2789
deepeshsaraswat <deepeshsaraswat02@gmail.com>
2023-09-04 17:05:56 +0530

    final

--- dbConnect.js
+++ dbConnect.js
@@ -1,14 +1,26 @@
-const mongoose=require('mongoose')
 
-const dbUrl="mongodb+srv://deepeshsaraswat:Deepesh1910@mytodo.hsc6xtt.mongodb.net/?retryWrites=true&w=majority"
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://deepeshsaraswat:Deepesh1910@mytodo.hsc6xtt.mongodb.net/?retryWrites=true&w=majority";
 
-const connectionParams={
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}
-
-mongoose.connect(dbUrl, connectionParams).then(()=>{
-    console.info("Connected to the DB")
-}).catch((e)=> {
-    console.log("Error:", e);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
